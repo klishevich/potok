13f59221dd79eaf639e4e3a6bc8e805f28920027
klishevich <busation@gmail.com>
2016-10-01 20:45:31 +0300

    capistrano

--- Gemfile
+++ Gemfile
@@ -30,6 +30,11 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'pg'
+gem 'capistrano', '~> 3.1', require: false
+gem 'capistrano-rails', '~> 1.1', require: false 
+gem 'capistrano-bundler', '~> 1.1', require: false
+gem 'capistrano-rvm', '~> 0.1', require: false 
+gem 'unicorn'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'potok'
+set :deploy_user, 'mike'
+
+set :filter, :roles => %w{app web}
+set :scm, :git
+set :repo_url, 'git@github.com:klishevich/potok.git'
+set :assets_roles, [:app]
+set :keep_releases, 5
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :tests, []
+set(:config_files, %w(
+  database.yml
+  unicorn_init.sh
+  secrets.yml
+))
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
+# set(:symlinks, [
+#   {
+    # source: "/home/mike/apps/{{full_app_name}}/shared/config/unicorn_init.sh",
+    # link: "/etc/init.d/unicorn_{{full_app_name}}"
+#   }
+# ])
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :groups, limit: 3, wait: 10 do
+      invoke 'unicorn:restart2'
+    end
+  end
+  after 'deploy:publishing', 'deploy:restart' 
+
+end
+
+namespace :unicorn do
+
+  desc 'Restart unicorn 2'
+  task :restart2 do
+    on roles(:app) do
+      execute "/etc/init.d/unicorn_potok_production restart"
+    end
+  end  
+
+end
