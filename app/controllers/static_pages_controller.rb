d20a837f76c0217f48a0882f0ce2a4f1e79c3c21
klishevich <busation@gmail.com>
2017-02-06 23:56:24 +0300

    improvements

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,41 +1,28 @@
 class StaticPagesController < ApplicationController
   def index
-  	@test = Test.new
-  	@NB_LOANS_ACTIVE_TOTAL = params[:NB_LOANS_ACTIVE_TOTAL].to_i
-  	@GRN_NUM = params[:GRN_NUM].to_i
-  	@NB_LOANS_ACTIVE_TOTAL0 = params[:NB_LOANS_ACTIVE_TOTAL0].to_i
-  	@TOTAL_INQUIRIES_12_MONTH1 = params[:TOTAL_INQUIRIES_12_MONTH1].to_i
-  	@REG_CODE = params[:REG_CODE]
-  	@FAMILY_CHLD_NUM = params[:FAMILY_CHLD_NUM]
-  	@WORK_SHARE_BUSINESS = params[:WORK_SHARE_BUSINESS]
-  	@res = -1
-  	if @NB_LOANS_ACTIVE_TOTAL
-  		@res = 0
-	  	@res = @NB_LOANS_ACTIVE_TOTAL< 4.5 ? @res + 1: @res
-	  	@res = @GRN_NUM<1 ? @res + 1 : @res
-	  	@res = @NB_LOANS_ACTIVE_TOTAL0 < 4.5 ? @res + 1: @res
-	  	@res = @TOTAL_INQUIRIES_12_MONTH1 < 4.5 ? @res + 1: @res
-	  	if @REG_CODE == '34' || @REG_CODE == '56' || @REG_CODE == '38' || @REG_CODE == '42' || @REG_CODE == '61' || @REG_CODE == '55' 
-	  		@res = @res
-	  	else
-	  		@res = @res + 0.5
-		  	@res = @WORK_SHARE_BUSINESS != '100'? @res + 0.5 : @res
-		end
+  	@married = params[:married].to_i
+  	@child_num = params[:child_num].to_i
+  	@active_loans = params[:active_loans]
+  	@inquiries = params[:inquiries]
+  	@grn_num = params[:grn_num]
+  	Rails.logger.info("married #{@married}")
+  	Rails.logger.info("child_num #{@child_num}")
+  	Rails.logger.info("active_loans #{@active_loans}")
+  	Rails.logger.info("inquiries #{@inquiries}")
+  	Rails.logger.info("grn_num #{@grn_num}")
+  	vFAMILY_MRTL_STATUS_NOT_M = @married == 0 ? 1 : 0
+  	vFAMILY_CHLD_NUM_EQ2 = @child_num >= 2 ? 1 : 0
+  	vNB_LOANS_ACTIVE_TOTAL_B = @active_loans == "B" ? 1 : 0
+  	vNB_LOANS_ACTIVE_TOTAL_C = @active_loans == "C" ? 1 : 0
+  	vnb_TOTAL_INQUIRIES_12_MONTH_B = @inquiries == "B" ? 1: 0
+  	vnb_TOTAL_INQUIRIES_12_MONTH_C = @inquiries == "C" ? 1 : 0
+  	vGRN_NUM = @grn_num
+  	logReg = LogReg.new
+  	if @grn_num != nil 
+	  	@rating = logReg.calc(vFAMILY_MRTL_STATUS_NOT_M, vFAMILY_CHLD_NUM_EQ2, vNB_LOANS_ACTIVE_TOTAL_B,
+	  		vNB_LOANS_ACTIVE_TOTAL_C, vnb_TOTAL_INQUIRIES_12_MONTH_B, vnb_TOTAL_INQUIRIES_12_MONTH_C, vGRN_NUM)
+		@good_rating = @rating > 0.3 ? true : false
 	end
-# 6  1  4  NULL  D  24.00000000  9  78  100.00
-
-  	@NB_LOANS_ACTIVE_TOTAL ||= 6
-  	@GRN_NUM ||=1
-  	@NB_LOANS_ACTIVE_TOTAL0 ||= 4
-  	@TOTAL_INQUIRIES_12_MONTH1 ||= 2
-  	@REG_CODE ||= '52'
-  	# @FAMILY_CHLD_NUM ||= ''
-  	@WORK_SHARE_BUSINESS ||= 100
-  	Rails.logger.info("GRN_NUM #{@GRN_NUM}")
-  	Rails.logger.info("FAMILY_CHLD_NUM #{@FAMILY_CHLD_NUM}")
-  	# cp = CalcProfitability.new
-  	# @invested_money ||= 0
-  	# @expect_profit = cp.expect_profit(@invested_money)
-  	# @expect_history_profit = cp.expect_history_profit(@invested_money)
   end
+
 end
